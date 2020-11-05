d76e3a1aa57ed6f350c6457dee6c6c9da1a97cac
谢志新 <xiezhixin@km.com>
2020-11-05 09:44:44 +0800

    update

---
+++ deploy.sh
@@ -0,0 +1,26 @@
+
+git checkout blogedit
+git add .
+git commit -m 'update'
+git push origin blogedit
+git status
+hugo -D
+
+
+
+cd public
+
+git add .
+git commit -m 'update post'
+git push github master
+git status
+cd ..
+
+
+cd public
+
+git add .
+git commit -m 'update post'
+git push origin master
+git status
+cd ..
