a1cacd21685e555b1d6ebb131eb8e3affdf4b385
谢志新 <xiezhixin@qimao.com>
2021-01-21 11:28:50 +0800

    update post

--- en.search.min.8c50e94dfec7fa1cfd7d73a190094ef3d0e4e94084b64962d165382ec1986771.js
+++ en.search.min.8c50e94dfec7fa1cfd7d73a190094ef3d0e4e94084b64962d165382ec1986771.js
@@ -2,7 +2,7 @@
 const characterPressed=String.fromCharCode(e.charCode);if(!isHotkey(characterPressed)){return;}
 input.focus();e.preventDefault();}
 function isHotkey(character){const dataHotkeys=input.getAttribute('data-hotkeys')||'';return dataHotkeys.indexOf(character)>=0;}
-function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/en.search-data.min.c531d0118155c54f37dacecfd45a1da3b7d161f4c1dc496facd22007ad259ac0.js',function(){input.required=false;search();});}
+function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/en.search-data.min.3e4322643b54623629cef6f050c0d92faafd9675e487451f0c0966d9f5bd0a4f.js',function(){input.required=false;search();});}
 function search(){while(results.firstChild){results.removeChild(results.firstChild);}
 if(!input.value){return;}
 const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=document.createElement('li'),a=li.appendChild(document.createElement('a'));a.href=page.href;a.textContent=page.title;results.appendChild(li);});}
