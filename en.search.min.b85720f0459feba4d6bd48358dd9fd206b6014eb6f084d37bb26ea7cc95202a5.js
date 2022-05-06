09ff3481287aad1a56b43dd9e48dc7a9ebab8b45
Brian Xie <yagoodidea@gmail.com>
2022-05-06 16:25:31 +0800

    update post

--- en.search.min.b85720f0459feba4d6bd48358dd9fd206b6014eb6f084d37bb26ea7cc95202a5.js
+++ en.search.min.b85720f0459feba4d6bd48358dd9fd206b6014eb6f084d37bb26ea7cc95202a5.js
@@ -2,7 +2,7 @@
 const characterPressed=String.fromCharCode(e.charCode);if(!isHotkey(characterPressed)){return;}
 input.focus();e.preventDefault();}
 function isHotkey(character){const dataHotkeys=input.getAttribute('data-hotkeys')||'';return dataHotkeys.indexOf(character)>=0;}
-function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/en.search-data.min.340f7f10f3991f3e45b1dba0df052759dec10bb6415db97aade995c52cd8c75c.js',function(){input.required=false;search();});}
+function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/en.search-data.min.c3110cabcbb0787219bfc6117a03b7966487f2c2acda80b9bf5ad50deba078d2.js',function(){input.required=false;search();});}
 function search(){while(results.firstChild){results.removeChild(results.firstChild);}
 if(!input.value){return;}
 const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=document.createElement('li'),a=li.appendChild(document.createElement('a'));a.href=page.href;a.textContent=page.title;results.appendChild(li);});}
